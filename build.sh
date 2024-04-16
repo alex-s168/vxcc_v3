2935eda8d66a2ad74eea805fff2f1272ee2b526e
alex-s168 <63254202+alex-s168@users.noreply.github.com>
2024-04-16 21:53:51 +0200

    add info step to ci

--- build.sh
+++ build.sh
@@ -8,6 +8,10 @@ CFLAGS="-Wall -Wextra -Wno-unused -Wpedantic -Werror -std=c11"
 if [[ $1 == "analyze" ]]; then
   echo "analyzing..."
   clang --analyze -Xclang -analyzer-werror $CFLAGS $FILES
+elif [[ $1 == "info" ]]; then
+  clang --version
+  echo cflags: $CFLAGS
+  echo files: $FILES
 else
   echo "compile DebugOpt"
   clang -g -ggdb -O3 $CFLAGS $FILES -o vxcc
