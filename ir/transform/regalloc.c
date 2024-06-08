f149af8e2a7b7572452e814a485ece228e3a1b9a
Devel <devellight@devellight.studio>
2024-06-08 19:19:59 +0200

    remove something

---
+++ ir/transform/regalloc.c
@@ -0,0 +1,8 @@
+#include "../ir.h"
+
+void vx_IrBlock_regalloc(vx_IrBlock * block) {
+    for (size_t i = 0; i < block->as_root.vars_len; i ++) {
+        lifetime l = block->as_root.vars[i].ll_lifetime;
+        if (l.first == NULL)
+            continue;
+
